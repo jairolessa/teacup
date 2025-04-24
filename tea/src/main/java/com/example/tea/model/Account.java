81566699ff39e48f743321edf1bfa0c84370580b
jairolessa <jairo.rafaelcarvalho@gmail.com>
2025-04-23 22:46:47 -0300

    Ajustando bug na listagem de responsáveis

--- tea/src/main/java/com/example/tea/model/Account.java
+++ tea/src/main/java/com/example/tea/model/Account.java
@@ -15,6 +15,48 @@ public class Account {
     @OneToOne
     private Responsible responsible;
 
+    public Account(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Responsible getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(Responsible responsible) {
+        this.responsible = responsible;
+    }
+
     public Account(String userName, String password, Boolean active, Responsible responsible) {
         this.userName = userName;
         this.password = password;
--- tea/src/main/java/com/example/tea/model/Dependent.java
+++ tea/src/main/java/com/example/tea/model/Dependent.java
@@ -2,6 +2,8 @@ package com.example.tea.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 public class Dependent extends Person{
 
@@ -10,4 +12,38 @@ public class Dependent extends Person{
 
     @ManyToOne
     private Responsible responsible;
+
+    public Dependent() {
+    }
+
+    public Dependent(String fullName, String cpf, LocalDate dateBirth, String nationality, Gender gender, String supportLevelTea, String medicalReport, Responsible responsible) {
+        super(fullName, cpf, dateBirth, nationality, gender);
+        this.supportLevelTea = supportLevelTea;
+        this.medicalReport = medicalReport;
+        this.responsible = responsible;
+    }
+
+    public String getSupportLevelTea() {
+        return supportLevelTea;
+    }
+
+    public void setSupportLevelTea(String supportLevelTea) {
+        this.supportLevelTea = supportLevelTea;
+    }
+
+    public String getMedicalReport() {
+        return medicalReport;
+    }
+
+    public void setMedicalReport(String medicalReport) {
+        this.medicalReport = medicalReport;
+    }
+
+    public Responsible getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(Responsible responsible) {
+        this.responsible = responsible;
+    }
 }
