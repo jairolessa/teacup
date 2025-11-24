48645a571f472c3a740e9f29688260141a5c8f24
jairolessa <jairo.rafaelcarvalho@gmail.com>
2025-11-23 23:30:29 -0300

    Configurando viwer dos dependentes cadastrados vinculados ao responsavel logado, edição do dependente, atualização e exclusão

--- demo/src/main/java/com/projetoTEA5/demo/controller/DependentController.java
+++ demo/src/main/java/com/projetoTEA5/demo/controller/DependentController.java
@@ -1,15 +1,13 @@
 package com.projetoTEA5.demo.controller;
 
 import com.projetoTEA5.demo.dto.DependentDto;
-import com.projetoTEA5.demo.model.Account;
+import com.projetoTEA5.demo.model.*;
 import com.projetoTEA5.demo.service.DependentService;
 import jakarta.validation.Valid;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/dependent")
@@ -22,7 +20,13 @@ public class DependentController {
     }
 
     @GetMapping("tea-cad")
-    public String registerDependent(){
+    public String registerDependent(Model model){
+
+        model.addAttribute("dependent", new Dependent());
+        model.addAttribute("genders", Gender.values());
+        model.addAttribute("bonds", Bond.values());
+        model.addAttribute("suportLevels", SuportLevels.values());
+
         return "tea-cad";
     }
 
@@ -31,6 +35,34 @@ public class DependentController {
                                    @AuthenticationPrincipal Account account){
 
         dependentService.saveDependent(dependentDto, account);
+
+        return "redirect:/responsible/portal";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editDependent(@PathVariable long id, Model model){
+
+        Dependent dependent = dependentService.findById(id);
+        model.addAttribute("dependent", dependent);
+        model.addAttribute("genders", Gender.values());
+        model.addAttribute("bonds", Bond.values());
+        model.addAttribute("suportLevels", SuportLevels.values());
+
+        return "tea-cad";
+    }
+
+    @PostMapping("/update")
+    public String updateDependent(@ModelAttribute("dependent") Dependent dependent){
+
+        dependentService.updateDependent(dependent);
+
+        return "redirect:/responsible/portal";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteDependent(@PathVariable Long id){
+
+        dependentService.deleteDependent(id);
 
         return "redirect:/responsible/portal";
     }
